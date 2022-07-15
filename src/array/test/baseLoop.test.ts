c17612d9e1c9718946b46f74aaabaea65b759a70
15881442478 <217516cs>
2022-07-15 14:32:12 +0800

    test

--- src/array/test/baseLoop.test.ts
+++ src/array/test/baseLoop.test.ts
@@ -1,13 +1,55 @@
-import { baseLoop } from '../baseLoop'
-import { describe, it, expect } from 'vitest'
+import { baseLoop, baseLoopRight, baseLoopAsync, baseLoopRightAsync } from '../baseLoop'
+import { describe, test, expect } from 'vitest'
+
+const arr = [1, 2, 3, 4, 5]
 
 describe('baseLoop', () => {
-  it('base', () => {
-    const arr = [1, 2, 3, 4, 5]
+  test('baseLoop function loop test', () => {
+    baseLoop(arr, (val, index) => {
+      expect(val).toBe(index + 1)
+    })
+  })
+
+  test('baseLoop function break loop test', () => {
     const result: number[] = []
-    baseLoop(arr, val => {
+    baseLoop(arr, (val, index, breakKey) => {
+      if (index === 3) {
+        return breakKey
+      }
       result.push(val)
     })
-    return expect(result).toEqual([1, 2, 3, 4, 5])
+    expect(result).toEqual([1, 2, 3])
+  })
+})
+
+describe('baseLoopRight', () => {
+  test('baseLoopRight function loop test', () => {
+    const result: number[] = []
+    baseLoopRight(arr, val => {
+      result.push(val)
+    })
+    expect(result).toEqual([5, 4, 3, 2, 1])
+  })
+})
+
+describe('baseLoopAsync', () => {
+  test('baseLoopAsync function asynchronous test', async () => {
+    const start = Date.now()
+    await baseLoopAsync([1, 2], async () => {
+      await new Promise(resolve => setTimeout(resolve, 200))
+    })
+    const end = Date.now()
+    expect(end - start).toBeGreaterThanOrEqual(400)
+  })
+})
+
+describe('baseLoopRightAsync', () => {
+  test('baseLoopRightAsync function asynchronous test', async () => {
+    const start = Date.now()
+    await baseLoopRightAsync([1, 2], async () => {
+      await new Promise(resolve => setTimeout(resolve, 200))
+    })
+    const end = Date.now()
+    expect(end - start).toBeGreaterThanOrEqual(400)
   })
 })
